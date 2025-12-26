59a5f50e103445aa8ca2a478cca67ce208a956d7
SK sanoyar ali <asksanoyar33@gmail.com>
2025-12-26 20:19:57 +0530

    initial client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+const App = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <button>click me</button>
+    </div>
+  )
+}
+
+export default App
